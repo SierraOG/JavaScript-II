0e798c25aa5323b29da3106688df438a1b76e2a8
SierraOG <sierrao@utexas.edu>
2019-06-11 15:04:07 -0500

    finished stretch problems

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -30,20 +30,36 @@ function getLength(arr, cb) {
   return cb(arr.length);
 }
 
+getLength([1,4,6,2,6], (len) => {
+  console.log(len);
+});
+
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
   return cb(arr[arr.length -1]);
 }
+
+last([1,4,6,2,6], (lastItem) => {
+  console.log(lastItem);
+});
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
   return cb(x + y);
 }
 
+sumNums(4, 5, (sum) => {
+  console.log(sum);
+});
+
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
   return cb(x*y);
 }
+
+multiplyNums(4, 5, (multi) => {
+  console.log(multi);
+});
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
@@ -51,10 +67,26 @@ function contains(item, list, cb) {
   return cb(list.includes(item));
 }
 
+contains('banana', ['kiwi','mango', 'banana'], (multi) => {
+  console.log(multi);
+});
+
 /* STRETCH PROBLEM */
 
 function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  newArray = array.sort();
+  for (i =1; i < array.length; i++){
+    if (newArray[i] === newArray[i-1]){
+      newArray.splice(i,1);
+    }
+  }
+  return cb(newArray);
 }
+
+array = [1,4,6,2,6];
+removeDuplicates(array, (newarr) => {
+  console.log(newarr);
+});
